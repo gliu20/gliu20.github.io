5f98ad3d33c363f8d51d3aca642d808625b90d23
George <gliu10000@gmail.com>
2018-01-24 15:54:29 -0500

    Update material-init.js

--- js/material-init.js
+++ js/material-init.js
@@ -1,4 +1,1 @@
 mdc.autoInit();
-
-let drawer = new mdc.drawer.MDCTemporaryDrawer(document.querySelector('.mdc-temporary-drawer'));
-document.querySelector('.menu').addEventListener('click', () => drawer.open = true);
