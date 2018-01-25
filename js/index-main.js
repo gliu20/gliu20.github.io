9abb97769d59bc5ccb2fc1111477518917b8ebd4
George <gliu10000@gmail.com>
2018-01-24 21:48:54 -0500

    Update index-main.js

--- js/index-main.js
+++ js/index-main.js
@@ -1,2 +1,2 @@
-let drawer = new mdc.drawer.MDCTemporaryDrawer(document.querySelector('.mdc-temporary-drawer'));
+let drawer = new mdc.drawer.MDCTemporaryDrawer(document.querySelector('.mdc-drawer--temporary'));
 document.querySelector('.menu').addEventListener('click', () => drawer.open = true);
