fc4738815f359074121b8a44025d55def841963a
George <gliu10000@gmail.com>
2018-02-02 14:23:03 -0500

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDh8qZhDqi38bhKiAPhlTxF7z4XDBTyfUM",
+  authDomain: "gliu20-bell.firebaseapp.com",
+  databaseURL: "https://gliu20-bell.firebaseio.com",
+  projectId: "gliu20-bell",
+  storageBucket: "",
+  messagingSenderId: "286893323970"
+};
+firebase.initializeApp(config);
