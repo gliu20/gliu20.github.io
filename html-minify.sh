3276b7544dabb6624ab85e8b4a868fcffeb2f0b2
George <24921711+gliu20@users.noreply.github.com>
2021-05-16 01:52:15 +0000

    finish minify script

--- html-minify.sh
+++ html-minify.sh
@@ -1,4 +1,9 @@
-find _site/* -type f -name "*.html" | while IFS= read -r INPUT_FILE; do
-    html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true $INPUT_FILE > $INPUT_FILE
+find _site/* -type f -name "*.html" | while read INPUT_FILE; do
+    echo "Minifying $INPUT_FILE"
+    html-minifier --collapse-whitespace --remove-comments \
+    --remove-optional-tags --remove-redundant-attributes \
+    --remove-script-type-attributes --remove-tag-whitespace \
+    --minify-css true --minify-js true \
+    $INPUT_FILE -o $INPUT_FILE
 done
 
