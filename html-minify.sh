fbd4da38800edd1e65856c04d55369953f6f41fd
George <24921711+gliu20@users.noreply.github.com>
2021-06-18 10:35:54 -0400

    keep optional tags
    it breaks google site verify

--- html-minify.sh
+++ html-minify.sh
@@ -1,7 +1,7 @@
 find docs/* -type f -name "*.html" | while read INPUT_FILE; do
     echo "Minifying $INPUT_FILE"
     html-minifier --collapse-whitespace --remove-comments \
-    --remove-optional-tags --remove-redundant-attributes \
+    --remove-redundant-attributes \
     --remove-script-type-attributes --remove-tag-whitespace \
     --minify-css true --minify-js true \
     $INPUT_FILE -o $INPUT_FILE
